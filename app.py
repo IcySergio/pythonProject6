6757ad63f46489cac727b6a886c7b945cf916e5a
IcySergio <106556861+IcySergio@users.noreply.github.com>
2023-12-01 21:29:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('final.html')
+# Пример данных
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-if __name__ == '__main__':
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/plots")
+def plots():
+    return render_template("plots.html", data=data)
+
+@app.route("/average", methods=["POST"])
+def average():
+    start = int(request.form.get("start"))
+    end = int(request.form.get("end"))
+    selected_data = data[start:end + 1]
+    avg = sum(selected_data) / len(selected_data)
+    return f"Average for selected range: {avg}"
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
